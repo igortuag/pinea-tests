8bab6b3d5e261dff40bbfa3945a097dba8b3acfe
Tuag <igortuag@gmail.com>
2023-10-24 15:09:24 -0300

    add state from pinea

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,6 +1,12 @@
+<script setup>
+import { useCounterStore } from '../stores/counter'
+const counterStore = useCounterStore()
+</script>
+
 <template>
   <div class="about">
     <h1>This is an about page</h1>
+    <p>Counter: {{ counterStore.count }}</p>
   </div>
 </template>
 
